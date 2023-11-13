5dc83d68d6781e937c37f978ec97641d8f1ffb25
Macce <makkeeh@gmail.com>
2023-11-13 13:08:38 +0200

    initial

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Azure_02_Web_API_Build_Pipeline;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
